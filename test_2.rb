6b4ff07706f768b9eea5517d71f39a17b943134b
yennanliu <f339339@gmail.com>
2025-07-02 15:30:50 +0800

    update

--- test_2.rb
+++ test_2.rb
@@ -4,4 +4,30 @@
 # 方法定義Methods
 
 
+def say_hello(name)
+  result = "Hi, " + name
+  return result
+end
+
+puts say_hello('ihower')
+# 輸出 Hi, ihower
+
+
+def hola(input)
+	return "input = #{input}"
+end
+
+puts hola("zzzzzz")
+
+
+def hello2(input="xxxx")
+	return "hi, " + input
+end
+
+puts hello2()
+puts hello2("jack")
+
+
 #----------------------
+
+
