915bb7147699577d18f3c52dc6c4bef1b77d3522
yennanliu <f339339@gmail.com>
2025-07-03 17:41:55 +0800

    add edit in controller, extract form to form html, update relative html

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -1,10 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update ]
+
   def index
     @products = Product.all
   end
 
+  # def show
+  #   @product = Product.find(params[:id])
+  # end
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -20,7 +24,37 @@ class ProductsController < ApplicationController
     end
   end
 
+  # def edit
+  #   @product = Product.find(params[:id])
+  # end
+
+  def edit
+  end
+
+  # def update
+  #   @product = Product.find(params[:id])
+  #   if @product.update(product_params)
+  #     redirect_to @product
+  #   elses
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+
     def product_params
       params.expect(product: [ :name ])
     end
