41d716484833c7d8c32c4ff99b0b13226e0b1b59
yennanliu <f339339@gmail.com>
2025-07-03 17:07:50 +0800

    add create product method controller

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -11,4 +11,18 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
+
 end
