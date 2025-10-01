d003723602df94c61a35b9539572776fcfcf953a
yennj12 <f339339@gmail.com>
2025-10-01 16:40:47 +0800

    Update cmd_edit_credentials.sh

--- cmd_edit_credentials.sh
+++ cmd_edit_credentials.sh
@@ -1,50 +1,59 @@
-  #Decode credentials to a temporary file:
+#Decode credentials to a temporary file:
 
-  # Make sure use ruby 3
-  eval "$(rbenv init -)"
-  source ~/.zshrc
+# V1
+eval "$(rbenv init -)"
+source ~/.zshrc
+# edit with sublime, can change to other IDE
+EDITOR="subl" rails credentials:edit
 
-  # Decode credentials to a file you can edit in your IDE
-  EDITOR="cat" rails credentials:show > /tmp/credentials_decrypted.yml
 
-  Edit the file in your IDE:
 
-  # Open in your preferred IDE/editor
-  code /tmp/credentials_decrypted.yml      # VSCode
-  cursor /tmp/credentials_decrypted.yml    # Cursor
-  open -a "Sublime Text" /tmp/credentials_decrypted.yml  # Sublime
-  # or just open it manually in your IDE
+# V2
+# Make sure use ruby 3
+eval "$(rbenv init -)"
+source ~/.zshrc
 
-  Re-encode after modification:
+# Decode credentials to a file you can edit in your IDE
+EDITOR="cat" rails credentials:show > /tmp/credentials_decrypted.yml
 
-  # Make sure use ruby 3
-  eval "$(rbenv init -)"
-  source ~/.zshrc
+Edit the file in your IDE:
 
-  # Option 1: Use a helper script to write the file back
-  EDITOR="cp /tmp/credentials_decrypted.yml" rails credentials:edit
+# Open in your preferred IDE/editor
+code /tmp/credentials_decrypted.yml      # VSCode
+cursor /tmp/credentials_decrypted.yml    # Cursor
+open -a "Sublime Text" /tmp/credentials_decrypted.yml  # Sublime
+# or just open it manually in your IDE
 
-  # Option 2: Use tee to pipe the content back
-  cat /tmp/credentials_decrypted.yml | EDITOR="tee" rails credentials:edit
+Re-encode after modification:
 
-  # Option 3: Use a shell script wrapper
-  EDITOR="/bin/bash -c 'cat /tmp/credentials_decrypted.yml > \$1'" rails credentials:edit
+# Make sure use ruby 3
+eval "$(rbenv init -)"
+source ~/.zshrc
 
-  Clean up:
+# Option 1: Use a helper script to write the file back
+EDITOR="cp /tmp/credentials_decrypted.yml" rails credentials:edit
 
-  # Remove the temporary decrypted file (IMPORTANT for security!)
-  rm /tmp/credentials_decrypted.yml
+# Option 2: Use tee to pipe the content back
+cat /tmp/credentials_decrypted.yml | EDITOR="tee" rails credentials:edit
 
-  Recommended workflow:
+# Option 3: Use a shell script wrapper
+EDITOR="/bin/bash -c 'cat /tmp/credentials_decrypted.yml > \$1'" rails credentials:edit
 
-  # 1. Decode
-  EDITOR="cat" rails credentials:show > /tmp/credentials_decrypted.yml
+Clean up:
 
-  # 2. Edit in your IDE (add the Twilio credentials)
-  code /tmp/credentials_decrypted.yml
+# Remove the temporary decrypted file (IMPORTANT for security!)
+rm /tmp/credentials_decrypted.yml
 
-  # 3. Re-encode (after saving your changes)
-  cat /tmp/credentials_decrypted.yml | EDITOR="tee" rails credentials:edit
+Recommended workflow:
 
-  # 4. Clean up
-  rm /tmp/credentials_decrypted.yml
+# 1. Decode
+EDITOR="cat" rails credentials:show > /tmp/credentials_decrypted.yml
+
+# 2. Edit in your IDE (add the Twilio credentials)
+code /tmp/credentials_decrypted.yml
+
+# 3. Re-encode (after saving your changes)
+cat /tmp/credentials_decrypted.yml | EDITOR="tee" rails credentials:edit
+
+# 4. Clean up
+rm /tmp/credentials_decrypted.yml
